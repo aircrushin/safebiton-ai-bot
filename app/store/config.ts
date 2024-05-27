71c8f062c2265bfe08973cf9969032d58145edf9
UltraV <1216564567@qq.com>
2024-05-27 10:43:00 +0800

    modify config.ts

--- app/store/config.ts
+++ app/store/config.ts
@@ -28,7 +28,7 @@ export enum Theme {
 export const DEFAULT_CONFIG = {
   lastUpdate: Date.now(), // timestamp, to merge state
 
-  submitKey: isMacOS() ? SubmitKey.MetaEnter : SubmitKey.CtrlEnter,
+  submitKey: isMacOS() ? SubmitKey.Enter : SubmitKey.Enter,
   avatar: "1f603",
   fontSize: 14,
   theme: Theme.Auto as Theme,
@@ -49,7 +49,7 @@ export const DEFAULT_CONFIG = {
     model: "safebition ai" as ModelType,
     temperature: 0.5,
     top_p: 1,
-    max_tokens: 4000,
+    max_tokens: 2000,
     presence_penalty: 0,
     frequency_penalty: 0,
     sendMemory: true,
