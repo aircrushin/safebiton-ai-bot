960017231a76bbc3a9a1860f22139631f47ccf76
UltraV <1216564567@qq.com>
2024-05-31 09:29:16 +0800

    更改baseurl

--- app/constant.ts
+++ app/constant.ts
@@ -11,7 +11,7 @@ export const RUNTIME_CONFIG_DOM = "danger-runtime-config";
 export const DEFAULT_CORS_HOST = "https://a.nextweb.fun";
 export const DEFAULT_API_HOST = `${DEFAULT_CORS_HOST}/api/proxy`;
 // export const OPENAI_BASE_URL = "https://api.openai.com";
-export const OPENAI_BASE_URL = "https://www.safebition.com/ai/";
+export const OPENAI_BASE_URL = "https://coze2openai-2cwm.onrender.com/";
 export enum Path {
   Home = "/",
   Chat = "/chat",
